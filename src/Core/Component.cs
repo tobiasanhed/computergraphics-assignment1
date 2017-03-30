92638eb938f8828df4c2af76c598d3876e6cd672
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-30 14:55:49 +0200

    component class

---
+++ src/Core/Component.cs
@@ -0,0 +1,9 @@
+namespace CG_A1 {
+
+/// <summary>Represents a single entity component.</summary>
+public abstract class Component {
+    // Placeholder class for components. Could also use an interface
+    // (IComponent).
+}
+
+}
