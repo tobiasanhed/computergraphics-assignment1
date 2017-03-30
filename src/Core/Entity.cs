d8ddaf705f67ab819d0e62fe86651afa99e573f1
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-30 15:02:46 +0200

    entity class

--- src/Core/Entity.cs
+++ src/Core/Entity.cs
@@ -1,9 +1,11 @@
-namespace CG_A1 {
+namespace CG_A1.Core {
 
 /*--------------------------------------
  * USINGS
  *------------------------------------*/
 
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
 /*--------------------------------------
@@ -12,12 +14,43 @@ using System.Threading;
 
 /// <summary>Represents a single entity.</summary>
 public sealed class Entity {
+    /*--------------------------------------
+     * PUBLIC PROPERTIES
+     *------------------------------------*/
+
     public int ID { get; }
+
+    /*--------------------------------------
+     * PRIVATE FIELDS
+     *------------------------------------*/
+
+    private readonly Dictionary<Type, Component> m_Components =
+        new Dictionary<Type, Component>();
 
     private static int s_ID = 1;
 
+    /*--------------------------------------
+     * CONSTRUCTOR
+     *------------------------------------*/
+
     public Entity() {
         ID = Interlocked.Increment(ref s_ID);
+    }
+
+    /*--------------------------------------
+     * PUBLIC METHODS
+     *------------------------------------*/
+
+    public void AddComponent<T>(T component) where T : Component {
+        m_Components.Add(typeof (T), component);
+    }
+
+    public T GetComponent<T>() where T : Component {
+        return (T)m_Components[typeof (T)];
+    }
+
+    public bool HasComponent<T>() where T : Component {
+        return m_Components.ContainsKey(typeof (T));
     }
 }
 
