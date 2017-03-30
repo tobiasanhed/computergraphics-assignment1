beed0c98a047dad1c51c298bbc0584378c5b3aaa
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-30 14:58:29 +0200

    working on entity class

--- src/Core/Component.cs
+++ src/Core/Component.cs
@@ -1,5 +1,9 @@
 namespace CG_A1 {
 
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
 /// <summary>Represents a single entity component.</summary>
 public abstract class Component {
     // Placeholder class for components. Could also use an interface
