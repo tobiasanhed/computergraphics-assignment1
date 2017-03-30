e03d9ba0ad7789cc459e785c884c0d2fa0254f81
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-30 15:11:43 +0200

    subsystem class

--- src/Core/Entity.cs
+++ src/Core/Entity.cs
@@ -18,6 +18,7 @@ public sealed class Entity {
      * PUBLIC PROPERTIES
      *------------------------------------*/
 
+    /// <summary>Gets the entity ID.</summary>
     public int ID { get; }
 
     /*--------------------------------------
@@ -33,6 +34,7 @@ public sealed class Entity {
      * CONSTRUCTOR
      *------------------------------------*/
 
+    /// <summary>Creates a new <see cref="Entity"/> instance.</summary>
     public Entity() {
         ID = Interlocked.Increment(ref s_ID);
     }
@@ -41,18 +43,26 @@ public sealed class Entity {
      * PUBLIC METHODS
      *------------------------------------*/
 
+    /// <summary>Adds the specified component to the entity.</summary>
+    /// <param name="component">The component to add to the entity.</param
     public void AddComponent<T>(T component) where T : Component {
         m_Components.Add(typeof (T), component);
     }
 
+    /// <summary>Destroys the entity by removing it from the scene.</summary>
     public void Destroy() {
         throw new System.NotImplementedException();
     }
 
+    /// <summary>Gets the entity component of the specified type.</summary>
     public T GetComponent<T>() where T : Component {
         return (T)m_Components[typeof (T)];
     }
 
+    /// <summary>Checks whether the entity has a component of the specified
+    ///          type.</summary>
+    /// <returns><see langword="true"/> if the entity has a component of the
+    ///          specified type.</returns>
     public bool HasComponent<T>() where T : Component {
         return m_Components.ContainsKey(typeof (T));
     }
