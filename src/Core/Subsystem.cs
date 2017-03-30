c8ca90b77577d4e84cf66d0b3996121d400816d5
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-30 15:11:46 +0200

    subsystem class

---
+++ src/Core/Subsystem.cs
@@ -0,0 +1,36 @@
+namespace CG_A1.Core {
+
+/*--------------------------------------
+ * USINGS
+ *------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using Microsoft.Xna.Framework;
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+/// <summary>Represents the base class for a single subsytem.</summary>
+public abstract class Subsystem {
+    /*--------------------------------------
+     * PUBLIC METHODS
+     *------------------------------------*/
+
+    /// <summary>Performs draw logic specified to the subsystem.</summary>
+    /// <param name="gameTime">The game time to take into consideration when
+    ///                        performing draw logic.</param
+    public virtual void Draw(GameTime gameTime) {
+    }
+
+    /// <summary>Performs update logic specified to the subsystem.</summary>
+    /// <param name="gameTime">The game time to take into consideration when
+    ///                        performing update logic.</param
+    public virtual void Update(GameTime gameTime) {
+    }
+}
+
+}
