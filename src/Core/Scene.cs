eb84571ca38649fb9f70f033c453dd7a4fb1ecce
Tobias Ånhed <Anhedtobias@gmail.com>
2017-03-30 15:16:22 +0200

    scene

---
+++ src/Core/Scene.cs
@@ -0,0 +1,32 @@
+namespace CG_A1 {
+
+/*--------------------------------------
+ * USINGS
+ *------------------------------------*/
+
+using System.Threading;
+using System.Collections.Generic;
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+/// <summary>Represents a single scene.</summary>
+public abstract class Scene {
+    private List<Subsystem> subsystems = new List<Subsystem>();
+    private List<Entity> entities      = new List<Entity>();
+
+    public virtual void Init(){ }
+    
+    public virtual CleanUp() { }
+
+    public void AddSubsystem(Subsystem s){
+        subsystems.Add(s);
+    }
+
+    public void AddEntities(int id, Entity e){
+        entities.Add(id, e);
+    }
+}
+
+}
