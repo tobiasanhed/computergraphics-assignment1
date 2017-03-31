629c927f0824775e8393f30c52c79289f781322a
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-31 13:13:40 +0200

    tweaks

--- src/Core/Subsystem.cs
+++ src/Core/Subsystem.cs
@@ -27,10 +27,18 @@ public abstract class Subsystem {
      * PUBLIC METHODS
      *------------------------------------*/
 
+    /// <summary>Performs cleanup logic.</summary>
+    public virtual void Cleanup() {
+    }
+
     /// <summary>Performs draw logic specific to the subsystem.</summary>
     /// <param name="t">The total game time, in seconds.</param>
     /// <param name="dt">The elapsed time since last call, in seconds.</param>
     public virtual void Draw(float t, float dt) {
+    }
+
+    /// <summary>Performs initialization logic.</summary>
+    public virtual void Init() {
     }
 
     /// <summary>Performs update logic specific to the subsystem.</summary>
@@ -38,10 +46,6 @@ public abstract class Subsystem {
     /// <param name="dt">The elapsed time since last call, in seconds.</param>
     public virtual void Update(float t, float dt) {
     }
-
-    public virtual void Init(){ }
-
-    public virtual void Cleanup(){ }
 }
 
 }
