5aec5eae611531bf8e81fda66da6865a23b617d6
Philip Arvidsson <philip@philiparvidsson.com>
2017-03-31 12:56:33 +0200

    restructuring stuff

---
+++ src/Subsystems/LogicSubsystem.cs
@@ -0,0 +1,38 @@
+namespace CG_A1.Subsystems {
+
+/*--------------------------------------
+ * USINGS
+ *------------------------------------*/
+
+using Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Components;
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+/// <summary>Represents a renderingsubsystem.</summary>
+public  class LogicSubsystem : Subsystem {
+    public override void Update(float t, float dt) {
+        var entities = Scene.GetEntities<LogicComponent>();
+
+        foreach (var entity in entities) {
+            var logic = entity.GetComponent<LogicComponent>();
+
+            var timer = logic.UpdateTimer + dt;
+
+            var invUpdateRate = logic.InvUpdateRate;
+            while (timer > invUpdateRate) {
+                logic.UpdateFunc(t, dt);
+                timer -= invUpdateRate;
+            }
+
+            logic.UpdateTimer = timer;
+        }
+    }
+}
+
+}
